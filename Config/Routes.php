c05a377348d0e8c88157709e3fc9fc7cda0e364c
basic-app <dev@basic-app.com>
2020-02-18 21:33:42 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -4,6 +4,6 @@
  * @license MIT
  * @link http://basic-app.com
  */
-$routes->add('admin/login', 'BasicApp\Admin\Controllers\Login::index');
-$routes->add('admin/logout', 'BasicApp\Admin\Controllers\Logout::index');
-$routes->add('admin/dashboard', 'BasicApp\Admin\Controllers\Dashboard::index');
+$routes->add('admin/login', '\BasicApp\Admin\Controllers\Login::index');
+$routes->add('admin/logout', '\BasicApp\Admin\Controllers\Logout::index');
+$routes->add('admin/dashboard', '\BasicApp\Admin\Controllers\Dashboard::index');
