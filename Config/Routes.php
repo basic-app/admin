6b8d0317a018fc3ab9d317f26ab86dbc9cd87499
basic-app <dev@basic-app.com>
2020-03-03 00:22:49 +0300

    n/a

--- Config/Routes.php
+++ Config/Routes.php
@@ -7,3 +7,7 @@
 $routes->add('admin/login', '\BasicApp\Admin\Controllers\Login::index');
 $routes->add('admin/logout', '\BasicApp\Admin\Controllers\Logout::index');
 $routes->add('admin/dashboard', '\BasicApp\Admin\Controllers\Dashboard::index');
+
+$routes->add('{locale}/admin/login', '\BasicApp\Admin\Controllers\Login::index');
+$routes->add('{locale}/admin/logout', '\BasicApp\Admin\Controllers\Logout::index');
+$routes->add('{locale}/admin/dashboard', '\BasicApp\Admin\Controllers\Dashboard::index');
