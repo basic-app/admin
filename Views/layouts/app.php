3f06d0e38493acb530a529f7f8ce8e3388c77d63
basic-app <dev@basic-app.com>
2026-04-07 21:53:23 +0300

    n/a

---
+++ Views/layouts/app.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @author Basic App Dev Team <dev@basic-app.com>
+ * @license MIT
+ * @link http://basic-app.com
+ */
+use BasicApp\Helpers\Url;
+use BasicApp\Admin\AdminEvents;
+
+$adminTheme = service('adminTheme');
+
+AdminEvents::registerAssets($adminTheme->head, $adminTheme->beginBody, $adminTheme->endBody);
+
+$actionMenu = array_key_exists('actionMenu', $this->data) ? $this->data['actionMenu'] : [];
+
+$request = service('request');
+
+$returnUrl = $request->getGet('returnUrl');
+
+if ($returnUrl)
+{
+    $actionMenu[] = [
+        'url'       => Url::createUrl($returnUrl),
+        'label'     => t('admin.menu', 'Back'),
+        'icon'      => 'fa fa-arrow-left',
+        'linkClass' => 'btn btn-secondary',
+    ];
+}
+
+$session = service('session');
+
+echo $adminTheme->mainLayout([
+    'homeUrl' => Url::createUrl('admin'),
+    'optionsMenu' => adminOptionsMenu($this->data['optionsMenu'] ?? []),
+    'mainMenu' => adminMainMenu($this->data['mainMenu'] ?? []),
+    'title' => array_key_exists('title', $this->data) ? $this->data['title'] : '',
+    'actionMenu' => $actionMenu,
+    'breadcrumbs' => array_key_exists('breadcrumbs', $this->data) ? $this->data['breadcrumbs'] : [],
+    'content' => $this->renderSection('content'),
+    'copyright' => 'Copyright © <a href="http://basic-app.com" target="_blank">Basic App</a> 2018 – ' . date('Y'),
+    'account' => [
+        'name' => user_id(),
+        'email' => null,
+        'description' => t('admin', 'Administrator'),
+        'avatarUrl' => null,
+        'profileUrl' => null,
+        'logoutUrl' => site_url('admin/logout'),
+        'logoutLabel' => t('admin', 'Logout')
+    ],
+    'successMessages' => (array) $session->getFlashdata('success'),
+    'errorMessages' => (array) $session->getFlashdata('error'),
+    'infoMessages' => (array) $session->getFlashdata('info')    
+]);
